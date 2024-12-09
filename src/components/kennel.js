850318e6aa15558926aefc3159f60933e1b1982b
Jean Patimeteeporn <jpatimet@gmail.com>
2024-12-09 01:25:13 -0600

    added starter code

---
+++ src/components/kennel.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { LocationList } from "./location-list/location";
+
+export const Kennel = () => {
+  <>
+    <h2>Nashville Kennels</h2>
+    <LocationList />
+  </>;
+};
