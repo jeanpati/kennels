ceda676cb71628e4b9b279f0121b56cbb2db4b69
Jean Patimeteeporn <jpatimet@gmail.com>
2025-01-02 17:42:04 -0600

    add navbar and application views components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Kennel } from "./components/kennel.js";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Kennel />
+    <Router>
+      <Kennel />
+    </Router>
   </React.StrictMode>
 );
