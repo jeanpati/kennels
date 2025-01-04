e3b06f2d63b5742a3e7b13fddcfec8a5df80061a
Jean Patimeteeporn <jpatimet@gmail.com>
2025-01-03 19:44:20 -0600

    added employee form and tested functionality

--- src/components/employee/employee-form.js
+++ src/components/employee/employee-form.js
@@ -1,6 +1,7 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { EmployeeContext } from "./employee-provider";
 import { LocationContext } from "../location/location-provider";
+import { useNavigate } from "react-router-dom";
 
 export const EmployeeForm = () => {
   const { addEmployee } = useContext(EmployeeContext);
@@ -11,4 +12,100 @@ export const EmployeeForm = () => {
     role: "",
     locationId: 0,
   });
+
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getLocations().then(setLoading(false));
+  }, [getLocations]);
+
+  const handleControlledInputChange = (event) => {
+    const newEmployee = { ...employee };
+    newEmployee[event.target.id] = event.target.value;
+    setEmployee(newEmployee);
+  };
+
+  const handleClickSaveEmployee = (event) => {
+    event.preventDefault(); //Prevents the browser from submitting the form
+
+    const locationId = parseInt(employee.locationId);
+
+    if (locationId === 0) {
+      window.alert("Please select a location and a customer");
+    } else {
+      //Invoke addemployee passing the new employee object as an argument
+      //Once complete, change the url and display the employee list
+
+      const newEmployee = {
+        name: employee.name,
+        role: employee.role,
+        locationId: locationId,
+      };
+      addEmployee(newEmployee).then(() => navigate("/employees"));
+    }
+  };
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <form className="employeeForm">
+      <h2 className="employeeForm__title">New Animal</h2>
+      <fieldset>
+        <div className="form-group">
+          <label htmlFor="name">Employee name:</label>
+          <input
+            type="text"
+            id="name"
+            required
+            autoFocus
+            className="form-control"
+            placeholder="Employee name"
+            value={employee.name}
+            onChange={handleControlledInputChange}
+          />
+        </div>
+      </fieldset>
+      <fieldset>
+        <div className="form-group">
+          <label htmlFor="name">Employee role:</label>
+          <input
+            type="text"
+            id="role"
+            required
+            autoFocus
+            className="form-control"
+            placeholder="Employee breed"
+            value={employee.role}
+            onChange={handleControlledInputChange}
+          />
+        </div>
+      </fieldset>
+      <fieldset>
+        <div className="form-group">
+          <label htmlFor="location">Assign to location: </label>
+          <select
+            name="locationId"
+            id="locationId"
+            className="form-control"
+            value={employee.locationId}
+            onChange={handleControlledInputChange}
+          >
+            <option value="0">Select a location</option>
+            {locations.map((l) => (
+              <option key={l.id} value={l.id}>
+                {l.name}
+              </option>
+            ))}
+          </select>
+        </div>
+      </fieldset>
+
+      <button className="btn btn-primary" onClick={handleClickSaveEmployee}>
+        Save Employee
+      </button>
+    </form>
+  );
 };
